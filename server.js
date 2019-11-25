aba6380416a7b927306758f9c60ce309d010685e
Jymma Karina Mogollón Cardoza <jkmogollon.cp@gmail.com>
2019-11-25 10:52:25 -0500

    cabeceras , metodos get y delete

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use('/', (req, res) => res.send('hola'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(router);
+
+router.get('/message', function (req, res) {
+    console.log(req.headers)
+    res.header({
+        "custom-header": "Nuestro valor personalizado",
+    })
+    res.send('Lista de mensajes');
+});
+router.delete('/message', function (req, res) {
+    console.log(req.query);
+    console.log(req.body);
+res.send(`Mensaje ${req.body.text} añadido correctamente`)
+});
+
+
 
 app.listen(3000, () => {
     console.log("server run!!!");

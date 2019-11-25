7e196e5a0ee310e6361a5494bc7001bb8ad9fa5b
Jymma Karina Mogollón Cardoza <jkmogollon.cp@gmail.com>
2019-11-25 11:14:45 -0500

    status 200

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ router.get('/message', function (req, res) {
     })
     res.send('Lista de mensajes');
 });
-router.delete('/message', function (req, res) {
+router.post('/message', function (req, res) {
     console.log(req.query);
     console.log(req.body);
-res.send(`Mensaje ${req.body.text} añadido correctamente`)
+res.status(201).send({error: '',body: 'Creado correctamente' })
 });
 
 
